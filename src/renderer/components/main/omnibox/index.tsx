9195e2e093eb5a6062dc5ce5f52666d66493bffc
David Singleton <davidsingleton@gmail.com>
2019-01-02 21:00:30 -0800

    Convert Omnibox to class and handle scrollTo

--- src/renderer/components/main/omnibox/index.tsx
+++ src/renderer/components/main/omnibox/index.tsx
@@ -8,34 +8,58 @@ import Main from '@renderer/containers/main';
 import Note from './note';
 
 
-/* SIDEBAR */
+/* OMNIBOX */
 
+class Omnibox extends React.Component<any> {
 
+  listRef: VariableSizeList;
 
-const Omnibox = ({ isOmni, setQuery, notes }) => {
+  constructor(props) {
+    super(props);
+    this.listRef = React.createRef();
+  }
 
-  const omnichange = (e) => {
-    console.log(e);
-    setQuery ( e.currentTarget.value );
-   };
+  componentDidMount () {
+    $.$window.on ( 'notes-scroll-to-item', this.scrollToItem );
+  }
 
-  if ( !isOmni ) return null;
+  componentWillUnmount () {
+    $.$window.off ( 'notes-scroll-to-item', this.scrollToItem );
+  }
 
-  return (
-    <div className="modal card bordered open show xs-7" id="omnibox-modal">
-      <form className="multiple joined no-separators grow">
-        <input autoFocus type="search" className="bordered grow" placeholder="🚀" defaultValue="" onInput={omnichange}/>
-        <div className="label bordered xsmall" title="Search">
-          <i className="icon">magnify</i>
-        </div>
-      </form>
-      <VariableSizeList overscanCount={3} className="layout-content react-window-list" height={200} width="auto" itemCount={notes.length} estimatedItemSize={20} itemSize={index => !index ? 28 : ( index === ( notes.length - 1 ) ? 23 : 28 )} itemKey={index => notes[index].filePath}>
-        {({ index, style }) => (
-          <Note style={style} note={notes[index]} />
-        )}
-      </VariableSizeList>
-    </div>
-  );
+  scrollToItem = ( event, index ) => {
+    console.log("omni scrollto " + index);
+    if ( !this.listRef.current ) return;
+    this.listRef.current.scrollToItem ( index, 'auto' );
+  };
+
+  render() {
+    const {isOmni, setQuery, notes} = this.props;
+
+    if ( !isOmni ) {
+      return null;
+    }
+  
+    const omnichange = (e) => {
+      setQuery ( e.currentTarget.value );
+    };
+
+    return (
+      <div className="modal card bordered open show xs-7" id="omnibox-modal">
+        <form className="multiple joined no-separators grow">
+          <input autoFocus type="search" className="bordered grow" placeholder="🚀" defaultValue="" onInput={omnichange} />
+          <div className="label bordered xsmall" title="Search">
+            <i className="icon">magnify</i>
+          </div>
+        </form>
+        <VariableSizeList ref={this.listRef} overscanCount={3} className="layout-content react-window-list" height={200} width="auto" itemCount={notes.length} estimatedItemSize={20} itemSize={index => !index ? 28 : ( index === ( notes.length - 1 ) ? 23 : 28 )} itemKey={index => notes[index].filePath}>
+          {({ index, style }) => (
+            <Note style={style} note={notes[index]} />
+          )}
+        </VariableSizeList>
+      </div>
+    );
+  }
 
 };
 
